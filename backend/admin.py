4f36ad85127b5ff845a2d5a62db9bd990deb282b
Iva Rakichevich <https://github.com/mikamurasaki2>
2024-05-17 17:33:50 +0300

    fix

--- backend/admin.py
+++ backend/admin.py
@@ -16,11 +16,17 @@ class User_admin(BaseModel):
 
 
 def get_all_users(db: Session = Depends(get_db)):
+    """
+    Функция получения из бд всех пользователей users_private
+    """
     return db.query(models.PrivateUser).distinct().all()
 
 
 def get_users(users: dict = Depends(get_all_users),
               token: str = Depends(reuseable_oauth)):
+    """
+    Функция для получения пользователей в панели админа (Управления пользователями) при наличии прав администратора
+    """
     is_admin = validate_token(token)
     if is_admin:
 
@@ -40,11 +46,16 @@ def get_users(users: dict = Depends(get_all_users),
 
 def delete_message(msg_id: int, token: str = Depends(reuseable_oauth),
                    db: Session = Depends(get_db)):
+    """
+    Функция удаления поста с проверкой на наличие прав администратора
+    """
     is_admin = validate_token(token)
     if is_admin:
         try:
+            # Находим пост по полю message_id
             post = db.query(models.Message).filter(models.Message.message_id == msg_id).first()
             if post:
+                # Удаляем пост
                 db.delete(post)
                 db.commit()
                 return {"message": "Message deleted successfully"}
@@ -59,10 +70,15 @@ def delete_message(msg_id: int, token: str = Depends(reuseable_oauth),
 
 def delete_reply(reply_id: int, token: str = Depends(reuseable_oauth), db: Session = Depends(get_db)):
     is_admin = validate_token(token)
+    """
+    Функция удаления комментария с проверкой на наличие прав администратора
+    """
     if is_admin:
         try:
+            # Находим комментарий по id (reply id msg)
             reply = db.query(models.Reply).filter(models.Reply.id == reply_id).first()
             if reply:
+                # Удаляем комментарий
                 db.delete(reply)
                 db.commit()
                 return {"message": "Reply deleted successfully"}
@@ -77,6 +93,9 @@ def delete_reply(reply_id: int, token: str = Depends(reuseable_oauth), db: Sessi
 
 def change_admin(user_in, token: str = Depends(reuseable_oauth),
                  db: Session = Depends(get_db)):
+    """
+    Функция для смены роли пользователя
+    """
     is_authenticated = validate_token(token)
     if is_authenticated:
         try:
--- backend/main.py
+++ backend/main.py
@@ -25,36 +25,55 @@ app.add_middleware(
 
 
 # USER
+
 @app.get("/api/get_messages")
 async def get_messages_route(posts: list = Depends(user.get_all_posts), token: str = Depends(reuseable_oauth),
                              db: Session = Depends(get_db)):
+    """
+    Эндпоинт для получения всех постов из бд
+    """
     return user.get_messages(posts, token, db)
 
 
 @app.get("/api/get_chats")
 async def get_chats_route(chats: list = Depends(user.get_all_chats), token: str = Depends(reuseable_oauth)):
+    """
+    Эндпоинт для получения всех чатов из бд
+    """
     return user.get_chats(chats, token)
 
 
 @app.get("/api/get_message/{msg_id}")
 async def get_message_route(post: dict = Depends(user.get_post), token: str = Depends(reuseable_oauth),
                             db: Session = Depends(get_db)):
+    """
+    Эндпоинт для получения конкретного поста
+    """
     return user.get_message(post, token, db)
 
 
 @app.get("/api/get_replies/{msg_id}")
 async def get_replies_route(replies: dict = Depends(user.get_replies), token: str = Depends(reuseable_oauth)):
+    """
+    Эндпоинт для получения всех комментариев из бд
+    """
     return user.get_replies_(replies, token)
 
 
 @app.post("/api/new_reply/")
 async def add_reply_route(reply: models.Reply_Insert, db: Session = Depends(get_db),
                           token: str = Depends(reuseable_oauth)):
+    """
+    Эндпоинт для отправки комментария из веб-приложения
+    """
     return user.add_reply(reply, db, token)
 
 
 @app.post("/api/login/")
 async def login_route(view_user: user.User, db: Session = Depends(get_db)):
+    """
+    Эндпоинт для авторизации
+    """
     return user.login(view_user, db)
 
 
@@ -62,21 +81,33 @@ async def login_route(view_user: user.User, db: Session = Depends(get_db)):
 
 @app.get("/api/get_users")
 async def get_users_route(users: dict = Depends(admin.get_all_users), token: str = Depends(reuseable_oauth)):
+    """
+    Эндпоинт для получения всех пользователей из бд в панели администратора (Управление пользователями)
+    """
     return admin.get_users(users, token)
 
 
 @app.delete("/api/delete_message/{msg_id}")
 async def delete_message_route(msg_id: int, token: str = Depends(reuseable_oauth),
                                db: Session = Depends(get_db)):
+    """
+    Эндпоинт для удаления поста
+    """
     return admin.delete_message(msg_id, token, db)
 
 
 @app.delete("/api/delete_reply/{reply_id}")
 async def delete_message_route(reply_id: int, token: str = Depends(reuseable_oauth), db: Session = Depends(get_db)):
+    """
+    Эндпоинт для удаления комментария
+    """
     return admin.delete_reply(reply_id, token, db)
 
 
 @app.post("/api/change_admin_status/")
 async def change_admin_route(user_in: admin.User_admin, token: str = Depends(reuseable_oauth),
                              db: Session = Depends(get_db)):
+    """
+    Эндпоинт для смены роли пользователя
+    """
     return admin.change_admin(user_in, token, db)
